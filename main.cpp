87365d9a38009ccdb6f1d8966bf6163e12d60176
Marco Bertoletti <100340284+MarcoBertoletti@users.noreply.github.com>
2022-05-14 19:06:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    char nome [10][20];
+    char n[20];
+    init (nome);
+    init (n);
+
+    for (int i=0; i<10; i++){
+        cin >>nome[i];
+    }
+
+    cin >>n;
+
+    int a=ricerca(nome,n);
+    if(a!=-1){
+        cout <<a;
+    }else{
+        cout<<"non presente\n";
+    }
+    return 0;
 }
